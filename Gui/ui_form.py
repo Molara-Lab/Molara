44b28dd44d80da67f5f0c399a03c4488a850e78c
Gereon Feldmann <feldmann.gereon@gmail.com>
2023-09-07 19:54:33 +0200

    added coord readin function

--- Gui/ui_form.py
+++ Gui/ui_form.py
@@ -25,23 +25,34 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
+
         MainWindow.resize(800, 600)
+
         self.quit = QAction(MainWindow)
         self.quit.setObjectName(u"quit")
+
         self.action_xyz = QAction(MainWindow)
         self.action_xyz.setObjectName(u"action_xyz")
+
+        self.action_coord = QAction(MainWindow)
+        self.action_coord.setObjectName(u"action_coord")
+
         self.actionCenter_Molecule = QAction(MainWindow)
         self.actionCenter_Molecule.setObjectName(u"actionCenter_Molecule")
+
         self.actionReset_View = QAction(MainWindow)
         self.actionReset_View.setObjectName(u"actionReset_View")
+
         self.actionto_x_axis = QAction(MainWindow)
         self.actionto_x_axis.setObjectName(u"actionto_x_axis")
         self.actionto_y_axis = QAction(MainWindow)
         self.actionto_y_axis.setObjectName(u"actionto_y_axis")
         self.actionto_z_axis = QAction(MainWindow)
         self.actionto_z_axis.setObjectName(u"actionto_z_axis")
+
         self.actionDraw_Axes = QAction(MainWindow)
         self.actionDraw_Axes.setObjectName(u"actionDraw_Axes")
+
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout = QGridLayout(self.centralwidget)
@@ -73,6 +84,7 @@ class Ui_MainWindow(object):
         self.menuFile.addAction(self.menuImport.menuAction())
         self.menuFile.addAction(self.quit)
         self.menuImport.addAction(self.action_xyz)
+        self.menuImport.addAction(self.action_coord)
         self.menuEdit.addAction(self.actionCenter_Molecule)
         self.menuEdit.addAction(self.actionReset_View)
         self.menuEdit.addAction(self.menuRotate.menuAction())
@@ -90,6 +102,7 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.quit.setText(QCoreApplication.translate("MainWindow", u"Quit", None))
         self.action_xyz.setText(QCoreApplication.translate("MainWindow", u".xyz", None))
+        self.action_coord.setText(QCoreApplication.translate("MainWindow", u"coord", None))
         self.actionCenter_Molecule.setText(QCoreApplication.translate("MainWindow", u"Center Molecule", None))
         self.actionReset_View.setText(QCoreApplication.translate("MainWindow", u"Reset View", None))
         self.actionto_x_axis.setText(QCoreApplication.translate("MainWindow", u"to x axis", None))
