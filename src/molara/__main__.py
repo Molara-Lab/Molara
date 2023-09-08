667a29ecd40490e33935240e90e26d6ddfa81e85
Alex Bonkowski <alexbonkowski@155-212.eduroam.rwth-aachen.de>
2023-09-08 13:49:19 +0200

    Run black.

--- src/molara/__main__.py
+++ src/molara/__main__.py
@@ -9,10 +9,10 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog
 #     pyside2-uic form.ui -o ui_form.py
 
 from .Gui.ui_form import Ui_MainWindow
-from .Molecule.Molecule import read_xyz,read_coord
+from .Molecule.Molecule import read_xyz, read_coord
+
 
 def main() -> None:
-
     format = QSurfaceFormat()
     format.setVersion(4, 1)
     format.setProfile(QSurfaceFormat.CoreProfile)
@@ -37,7 +37,7 @@ def main() -> None:
             fileName = QFileDialog.getOpenFileName(self, "Open .xyz file", "/home", "Image Files (*.xyz)")
             mol = read_xyz(fileName[0])
             widget.ui.openGLWidget.set_molecule(mol)
-            
+
         def show_coord(self):
             fileName = QFileDialog.getOpenFileName(self, "Open coord file", "/home", "Image Files (*)")
             mol = read_coord(fileName[0])
@@ -45,7 +45,7 @@ def main() -> None:
 
     app = QApplication(sys.argv)
     widget = MainWindow()
-    widget.setWindowTitle('Molara')
+    widget.setWindowTitle("Molara")
     widget.show()
 
     if len(sys.argv) > 1:
@@ -58,5 +58,6 @@ def main() -> None:
     widget.ui.quit.triggered.connect(widget.close)
     sys.exit(app.exec())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
