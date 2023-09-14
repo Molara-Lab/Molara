e1f6ebb59d50953de615f9a3d65b15a75ea7042a
Alexander Bonkowski <alexander.bonkowski@rwth-aachen.de>
2023-09-14 15:04:49 +0200

    Add test

---
+++ tests/molara/Molecule/test_Crystal.py
@@ -0,0 +1,30 @@
+import numpy as np
+from numpy.testing import assert_array_equal
+from unittest import TestCase
+
+from molara.Molecule.Crystal import Crystal
+
+class TestCrystal(TestCase):
+    def setUp(self):
+        atomic_numbers = [5, 7]
+        coordinates = [[0.0, 0.0, 0.0], [0.25, 0.25, 0.25]]
+        basis_vectors = [[0.0, 1.785, 1.785], [1.785, 0.0, 1.785], [1.785, 1.785, 0.0]]
+        self.crystal = Crystal(atomic_numbers, coordinates, basis_vectors)
+
+        assert len(self.crystal.atoms) == 2
+        assert_array_equal(self.crystal.basis_vectors, basis_vectors)
+        assert_array_equal(self.crystal.atomic_numbers, atomic_numbers)
+        assert_array_equal(self.crystal.coordinates, coordinates)
+
+    def test_from_POSCAR(self):
+        self.crystal_from_POSCAR = Crystal.from_POSCAR("examples/POSCAR/boron_nitride")
+
+        assert len(self.crystal_from_POSCAR.atoms) == 2
+        assert_array_equal(self.crystal_from_POSCAR.basis_vectors, self.crystal.basis_vectors)
+        assert_array_equal(self.crystal_from_POSCAR.atomic_numbers, self.crystal.atomic_numbers)
+        assert_array_equal(self.crystal_from_POSCAR.coordinates, self.crystal.coordinates)
+        #assert self.crystal_from_POSCAR == self.crystal
+    
+    # def test_make_supercell(self):
+    #     self.crystal.make_supercell([2, 2, 2])
+    #     assert len(self.crystal.atoms) == 16
