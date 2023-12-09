f52e5fcbd0d983f55dcf87b5c061cd512043f18c
Adrian Usler <adrian.usler@googlemail.com>
2023-12-09 18:51:04 +0100

    Implement creation of periodic atoms at edges in supercell

--- src/molara/Molecule/crystal.py
+++ src/molara/Molecule/crystal.py
@@ -23,10 +23,10 @@ class Crystal(Molecule):
     Particle positions are given in terms of the basis vectors:
     E.g. the position (0.5, 0.5, 0.) is always the center of a unit cell wall, regardless of the crystal system.
 
-    :param atomic_numbers: contains the atomic numbers of the particles specified for the unit cell.
-    :type atomic_numbers: numpy.array of int
-    :param coordinates: Nx3 matrix of particle coordinates in the unit cell, in terms of the basis vectors.
-    :type coordinates: numpy.ndarray of numpy.float64
+    :param atomic_nums: contains the atomic numbers of the particles specified for the unit cell.
+    :type atomic_nums: numpy.array of int
+    :param coords: Nx3 matrix of particle coordinates in the unit cell, in terms of the basis vectors.
+    :type coords: numpy.ndarray of numpy.float64
     :param basis_vectors: 3x3 matrix of the lattice basis vectors.
     :type basis_vectors: numpy.ndarray of numpy.float64
     :param supercell_dimensions: side lengths of the supercell in terms of the cell constants
@@ -35,22 +35,22 @@ class Crystal(Molecule):
 
     def __init__(
         self,
-        atomic_numbers: Sequence[int],
-        coordinates: Sequence[Sequence[float]],
+        atomic_nums: Sequence[int],
+        coords: Sequence[Sequence[float]],
         basis_vectors: Sequence[Sequence[float]] | ArrayLike,
     ) -> None:
         """Creates a crystal supercell based on given particle positions in unit cell and lattice basis vectors."""
-        self.atomic_numbers_unitcell = atomic_numbers
-        self.coordinates_unitcell = coordinates
+        self.atomic_nums_unitcell = atomic_nums
+        self.coords_unitcell = coords
         self.basis_vectors = basis_vectors
-        self.make_supercell([1, 1, 1])
+        self.make_supercell([3,3,3])
 
     def make_supercell(self, supercell_dimensions: Annotated[Sequence, 3]) -> None:
         """Creates a supercell of the crystal."""
         self.supercell_dimensions = supercell_dimensions
-        steps_a = np.arange(supercell_dimensions[0] + 1)
-        steps_b = np.arange(supercell_dimensions[1] + 1)
-        steps_c = np.arange(supercell_dimensions[2] + 1)
+        steps_a = np.arange(supercell_dimensions[0])
+        steps_b = np.arange(supercell_dimensions[1])
+        steps_c = np.arange(supercell_dimensions[2])
         steps_a.shape = (*steps_a.shape, 1, 1)
         steps_b.shape = (1, *steps_b.shape, 1)
         steps_c.shape = (1, 1, *steps_c.shape)
@@ -66,55 +66,129 @@ class Crystal(Molecule):
         ).T
 
         num_unit_cells = translation_vectors.shape[0]
-        self.fractional_coordinates_supercell = np.empty((0, 3), float)
-        self.atomic_numbers_supercell = np.empty(0, int)
+        self.fractional_coords_supercell = np.empty((0, 3), float)
+        self.atomic_nums_supercell = np.empty(0, int)
+        
+        # create supercell arrays
         for atomic_number_i, position_i in zip(
-            self.atomic_numbers_unitcell,
-            self.coordinates_unitcell,
+            self.atomic_nums_unitcell,
+            self.coords_unitcell,
         ):
-            self.atomic_numbers_supercell = np.append(
-                self.atomic_numbers_supercell,
+            self.atomic_nums_supercell = np.append(
+                self.atomic_nums_supercell,
                 [atomic_number_i] * num_unit_cells,
             )
-            self.fractional_coordinates_supercell = np.append(
-                self.fractional_coordinates_supercell,
+            self.fractional_coords_supercell = np.append(
+                self.fractional_coords_supercell,
                 position_i + translation_vectors,
                 axis=0,
             )
-
-        # remove positions outside of the supercell box
-        ids_remove_a = np.where(
-            self.fractional_coordinates_supercell[:, 0] > supercell_dimensions[0],
+        
+        # create extra atoms at edges of supercell (quasi periodic boundaries)
+        extra_atomic_nums, extra_fractional_coords = Crystal.make_supercell_edge_atoms(
+            self.atomic_nums_supercell,
+            self.fractional_coords_supercell,
+            supercell_dimensions
         )
-        ids_remove_b = np.where(
-            self.fractional_coordinates_supercell[:, 1] > supercell_dimensions[1],
-        )
-        ids_remove_c = np.where(
-            self.fractional_coordinates_supercell[:, 2] > supercell_dimensions[2],
-        )
-        ids_remove = np.unique(
-            np.concatenate((ids_remove_a, ids_remove_b, ids_remove_c)),
-        )
-        self.fractional_coordinates_supercell = np.delete(
-            self.fractional_coordinates_supercell,
-            ids_remove,
-            axis=0,
-        )
-        self.atomic_numbers_supercell = np.delete(
-            self.atomic_numbers_supercell,
-            ids_remove,
-        )
-
-        # transform fractional to cartesian coordinates and instantiate atoms in super().__init__
-        self.cartesian_coordinates_supercell = np.dot(
-            self.fractional_coordinates_supercell,
+        self.atomic_nums_supercell = np.append(self.atomic_nums_supercell, extra_atomic_nums)
+        self.fractional_coords_supercell = np.append(self.fractional_coords_supercell, extra_fractional_coords, axis=0)
+        
+        # transform fractional to cartesian coords ...
+        self.cartesian_coords_supercell = np.dot(
+            self.fractional_coords_supercell,
             self.basis_vectors,
         )
+        # ... and instantiate atoms in super().__init__
         super().__init__(
-            self.atomic_numbers_supercell,
-            self.cartesian_coordinates_supercell,
+            self.atomic_nums_supercell,
+            self.cartesian_coords_supercell,
         )
+    
+    @classmethod
+    def make_supercell_edge_atoms(
+        cls: type[Crystal],
+        atomic_nums: Sequence[float],
+        fractional_coords: Sequence[Sequence[float]],
+        supercell_dims: Sequence[int]
+    ):
+        """ if an atom is on one (or more) of the unit cell boundaries,
+        it shall be mirrored to the other side. Effectively, this means
+        that any atom that has a fractional coordinate 0 should get one
+        with 1 extra.
+        
+        :param atomic_nums: atomic numbers of the atoms
+        :param fractional_coords: fractional coordinates,
+          i.e., the coordinates of the atoms in terms of the basis vectors.
+        :param supercell_dims: supercell dimensions, e.g., [3,2,5] for a 3x2x5 supercell."""
+        
+        # first get 2D info on where the zero-valued atom coordinates are located.
+        # ids_edge_atoms contains the 0th-axis ids, i.e., the ids of the atoms with zero-valued coords.
+        # ids_edge_atom_coords contains the 1st-axis ids, i.e., the ids of the specific coords that are zero-valued.
+        # example: if there are five atoms, the 0th and the 3rd of which have coordinates (0, .5, .5) and (.5, 0, 0),
+        # respectively, then ids_edge_atoms would be [0, 3, 3], and ids_edge_atom_coords would be [0, 1, 2].
+        _fractional_coords_np = np.array(fractional_coords)
+        _supercell_dims_np = np.array(supercell_dims)
+        ids_edge_atoms, ids_edge_atom_coords = np.where(_fractional_coords_np==0)
+        extra_atomic_nums = []# atomic numbers of the newly created atoms
+        extra_fractional_coords = []
 
+        # iterate over the relevant atoms
+        for _id_atom_unique in np.unique(ids_edge_atoms):
+            _atomic_num = atomic_nums[_id_atom_unique]
+            # get the zero-valued coord ids of the respective atom
+            _ids_atom_coords = ids_edge_atom_coords[ids_edge_atoms==_id_atom_unique]
+            _atom_coords = _fractional_coords_np[_id_atom_unique].copy()
+            if len(_ids_atom_coords)==1:# e.g., (.5, 0, .5)
+                id1 = _ids_atom_coords[0]
+                dim1 = _supercell_dims_np[id1]
+                _atom_coords[id1] = dim1
+                extra_fractional_coords += [_atom_coords]
+                extra_atomic_nums += [_atomic_num]
+            elif len(_ids_atom_coords)==2:# e.g., (.5, 0, 0)
+                id1, id2 = _ids_atom_coords
+                dim1, dim2 = _supercell_dims_np[_ids_atom_coords]
+                _atom_coords[id1] = dim1# (dim1,0)
+                extra_fractional_coords += [_atom_coords]
+                _atom_coords = _atom_coords.copy()
+                _atom_coords[id2] = dim2# (dim1,dim2)
+                extra_fractional_coords += [_atom_coords]
+                # reset
+                _atom_coords = _fractional_coords_np[_id_atom_unique].copy()
+                _atom_coords[id2] = dim1# (0,dim1)
+                extra_fractional_coords += [_atom_coords]
+                extra_atomic_nums += [_atomic_num]*3
+            elif len(_ids_atom_coords)==3:# i.e., (0, 0, 0)
+                id1, id2, id3 = _ids_atom_coords
+                dim1, dim2, dim3 = _supercell_dims_np[_ids_atom_coords]
+                _atom_coords[id1] = dim1#(dim1,0,0)
+                extra_fractional_coords += [_atom_coords]
+                _atom_coords = _atom_coords.copy()
+                _atom_coords[id2] = dim2#(dim1,dim2,0)
+                extra_fractional_coords += [_atom_coords]
+                _atom_coords = _atom_coords.copy()
+                _atom_coords[id3] = dim3#(dim1,dim2,dim3)
+                extra_fractional_coords += [_atom_coords]
+                # reset
+                _atom_coords = _fractional_coords_np[_id_atom_unique].copy()
+                _atom_coords[id1] = dim1
+                _atom_coords[id3] = dim3# (dim1,0,dim3)
+                extra_fractional_coords += [_atom_coords]
+                # reset
+                _atom_coords = _fractional_coords_np[_id_atom_unique].copy()
+                _atom_coords[id2] = dim2# (0,dim2,0)
+                extra_fractional_coords += [_atom_coords]
+                _atom_coords = _atom_coords.copy()
+                _atom_coords[id3] = dim3# (0,dim2,dim3)
+                extra_fractional_coords += [_atom_coords]
+                # reset
+                _atom_coords = _fractional_coords_np[_id_atom_unique].copy()
+                _atom_coords[id3] = dim3# (0,0,dim3)
+                extra_fractional_coords += [_atom_coords]
+                extra_atomic_nums += [_atomic_num]*7
+            else:
+                raise(ValueError)
+        return extra_atomic_nums, extra_fractional_coords
+        
     @classmethod
     def from_poscar(cls: type[Crystal], file_path: str) -> Crystal:
         """Creates a Crystal object from a POSCAR file."""
@@ -145,9 +219,9 @@ class Crystal(Molecule):
         if mode.lower() != "direct":
             msg = "Currently, Molara can only process direct mode in POSCAR files."
             raise NotImplementedError(msg)
-        atomic_numbers = [element_symbol_to_atomic_number(symb) for symb in species]
+        atomic_nums = [element_symbol_to_atomic_number(symb) for symb in species]
         return cls(
-            atomic_numbers,
+            atomic_nums,
             positions,
             [scale * np.array(bv, dtype=float) for bv in basis_vectors],
         )
@@ -156,8 +230,8 @@ class Crystal(Molecule):
         """Returns a copy of the Crystal object."""
         # supercell dimensions not included yet!
         return Crystal(
-            self.atomic_numbers_unitcell,
-            self.coordinates_unitcell,
+            self.atomic_nums_unitcell,
+            self.coords_unitcell,
             self.basis_vectors,
         )
 
