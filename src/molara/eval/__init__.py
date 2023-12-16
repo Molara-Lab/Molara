f688a512e0473f9e47104469ab3cbae81aed0580
Michel Heinz <heinz@pc.rwth-aachen.de>
2023-12-16 21:12:00 +0100

    Start eval module

---
+++ src/molara/eval/__init__.py
@@ -0,0 +1,1 @@
+"""Initialize the eval module."""
---
+++ src/molara/eval/aos.py
@@ -0,0 +1,21 @@
+"""This module serves the calculation of atomic orbitals."""
+
+from __future__ import annotations
+
+
+# def ao_gto(
+#     exponents: list[float],
+#     coefficients: list[float],
+#     x: float,
+#     y: float,
+#     z: float,
+# ) -> float:
+#     """Calculates the value of a gaussian type atomic orbital at a given point.
+#
+#     :param exponents: list of exponents
+#     :param coefficients: list of coefficients
+#     :param x: x coordinate
+#     :param y: y coordinate
+#     :param z: z coordinate
+#     :return: value of the gaussian type orbital at the given point
+#     """
