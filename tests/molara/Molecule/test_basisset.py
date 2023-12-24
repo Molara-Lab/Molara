592da4a89e2ee18915d4fb09fad229d3c725e820
Michel Heinz <heinz@pc.rwth-aachen.de>
2023-12-24 16:31:51 +0100

    Debug test

--- tests/molara/Molecule/test_basisset.py
+++ tests/molara/Molecule/test_basisset.py
@@ -58,6 +58,7 @@ class TestBasisset(TestCase):
                     np.array([0.0, 0.0, 0.0]),
                     np.array([0.0, 0.0, 0.0]),
                 )
+        print(overlap_matrix - self.correct_matrix)
         assert_array_almost_equal_nulp(overlap_matrix, self.correct_matrix, 1e-14)
 
 
