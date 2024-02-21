d659c204f4a8c18bf6bdb5be6b228bf6e248fc49
Adrian Usler <adrian.usler@googlemail.com>
2024-02-21 13:22:13 +0100

    shift tests in setUp routine to new test_setup routine

--- tests/molara/Molecule/test_Crystal.py
+++ tests/molara/Molecule/test_Crystal.py
@@ -17,19 +17,21 @@ class TestCrystal(TestCase):
 
     def setUp(self) -> None:
         """Set up a crystal."""
-        atomic_numbers = [5, 7]
-        coordinates = [[0.0, 0.0, 0.0], [0.25, 0.25, 0.25]]
-        basis_vectors = [[0.0, 1.785, 1.785], [1.785, 0.0, 1.785], [1.785, 1.785, 0.0]]
+        self.atomic_numbers = [5, 7]
+        self.coordinates = [[0.0, 0.0, 0.0], [0.25, 0.25, 0.25]]
+        self.basis_vectors = [[0.0, 1.785, 1.785], [1.785, 0.0, 1.785], [1.785, 1.785, 0.0]]
         self.supercell_dims = [2, 7, 4]
-        self.crystal = Crystal(atomic_numbers, coordinates, basis_vectors, self.supercell_dims)
+        self.crystal = Crystal(self.atomic_numbers, self.coordinates, self.basis_vectors, self.supercell_dims)
+        
+    def test_setup(self) -> None:
         supercell_dims = self.crystal.supercell_dims
         assert len(self.crystal.atoms) == (
             (supercell_dims[0] + 1) * (supercell_dims[1] + 1) * (supercell_dims[2] + 1)
             + supercell_dims[0] * supercell_dims[1] * supercell_dims[2]
         )
-        assert_array_equal(self.crystal.basis_vectors, basis_vectors)
-        assert_array_equal(self.crystal.atomic_nums_unitcell, atomic_numbers)
-        assert_array_equal(self.crystal.coords_unitcell, coordinates)
+        assert_array_equal(self.crystal.basis_vectors, self.basis_vectors)
+        assert_array_equal(self.crystal.atomic_nums_unitcell, self.atomic_numbers)
+        assert_array_equal(self.crystal.coords_unitcell, self.coordinates)
 
     def test_from_poscar(self) -> None:
         """Test the creation of a crystal from a POSCAR file."""
