207df2164d0f71fc60f974913b33fd5b36d3ca4b
Florian Solbach <florian.solbach@rwth-aachen.de>
2024-02-29 20:33:38 +0100

    Add logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,8 +24,23 @@ templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
+# -- General configuration ---------------------------------------------------
+# grab the release, i.e. the full version, including alpha/beta/rc tags.
+release = "0.0.1"
+# to distinguish X.Y version (assume PEP 440 compliant versioning) from release,
+# uncomment the following:
+# version = re.match(r"(\d+!)?(\d+\.\d+).*", release).group(2)
+# Here, we use the full version as the version number:
+version = release
+
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+html_logo = "_static/logo.png"
+html_theme_options = {
+    'logo_only': False,      # only display the logo not the project name
+    'display_version': True, # display the version number in the sidebar
+}
