a7823d1738b34236cc3eb0f650a94b8dc62400d0
Adrian Usler <adrian.usler@googlemail.com>
2024-03-07 14:27:28 +0100

    Solve fatal error in test_draw_cylinders: work with qtbot

--- tests/molara/Rendering/test_rendering.py
+++ tests/molara/Rendering/test_rendering.py
@@ -2,17 +2,32 @@
 
 from __future__ import annotations
 
-import unittest
+from typing import TYPE_CHECKING
 
+import numpy as np
 from molara.Rendering.rendering import Renderer
+from PySide6.QtOpenGLWidgets import QOpenGLWidget
+
+if TYPE_CHECKING:
+    from pytestqt.qtbot import QtBot
 
 
-class TestRenderer(unittest.TestCase):
+def test_renderer(qtbot: QtBot) -> None:
+    """Tests the Renderer class."""
+    workaround_test_renderer = WorkaroundTestRenderer(qtbot)
+    workaround_test_renderer.test_init()
+    workaround_test_renderer.test_draw_cylinders()
+
+
+class WorkaroundTestRenderer:
     """This class contains the tests for the Renderer class."""
 
-    def setUp(self) -> None:
+    def __init__(self, qtbot: QtBot) -> None:
         """Instantiates the Renderer object."""
+        self.qtbot = qtbot
+        self.openGLWidget = QOpenGLWidget(None)
         self.renderer = Renderer()
+        self.openGLWidget.show()
 
     def test_init(self) -> None:
         """Tests the __init__ method of the Renderer class."""
@@ -23,17 +38,26 @@ class TestRenderer(unittest.TestCase):
         assert isinstance(self.renderer.cylinders, list)
         assert self.renderer.shader == 0
 
-    # def test_draw_cylinders(self) -> None:
-    #     positions = np.array([[0, 0, 0], [1, 1, 1]])
-    #     directions = np.array([[1, 0, 0], [0, 1, 0]])
-    #     radii = np.array([0.5, 0.3])
-    #     lengths = np.array([1.0, 2.0])
-    #     colors = np.array([[1, 0, 0], [0, 1, 0]])
-    #     subdivisions = 10
+    def test_draw_cylinders(self) -> None:
+        """Tests the draw_cylinders method of the Renderer class."""
+        positions = np.array([[0, 0, 0], [1, 1, 1]], dtype=np.float32)
+        directions = np.array([[1, 0, 0], [0, 1, 0]], dtype=np.float32)
+        radii = np.array([0.5, 0.3], dtype=np.float32)
+        lengths = np.array([1.0, 2.0], dtype=np.float32)
+        colors = np.array([[1, 0, 0], [0, 1, 0]], dtype=np.float32)
+        subdivisions = 10
 
-    #     result = self.renderer.draw_cylinders(positions, directions, radii, lengths, colors, subdivisions)
-
-    #     self.assertEqual(result, 2)  # Assert that the number of cylinders drawn is correct
+        self.openGLWidget.makeCurrent()
+        count_cylinder_instances = 0
+        result = self.renderer.draw_cylinders(positions, directions, radii, lengths, colors, subdivisions)
+        assert result == count_cylinder_instances
+        count_cylinder_instances += 1
+        result = self.renderer.draw_cylinders(positions, directions, radii, lengths, colors, subdivisions)
+        assert result == count_cylinder_instances
+        count_cylinder_instances += 1
+        result = self.renderer.draw_cylinders(positions, directions, radii, lengths, colors, subdivisions)
+        assert result == count_cylinder_instances
+        self.openGLWidget.doneCurrent()
 
     # def test_draw_spheres(self):
     #     positions = np.array([[0, 0, 0], [1, 1, 1]])
