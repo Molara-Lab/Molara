d55d2080a23b253364f1621045ea0cc910bfb9e5
Adrian Usler <adrian.usler@googlemail.com>
2024-03-08 04:19:48 +0100

    Add test_set_shader to test_rendering

--- tests/molara/Rendering/test_rendering.py
+++ tests/molara/Rendering/test_rendering.py
@@ -47,6 +47,12 @@ class WorkaroundTestRenderer:
         assert isinstance(self.renderer.cylinders, list)
         assert self.renderer.shader == 0
 
+    def test_set_shader(self) -> None:
+        """Tests the set_shader method of the Renderer class."""
+        shader_int = 192837465
+        self.renderer.set_shader(shader_int)
+        assert self.renderer.shader == shader_int
+
     def test_draw_cylinders(self) -> None:
         """Tests the draw_cylinders method of the Renderer class."""
         positions = np.array([[0, 0, 0], [1, 1, 1]], dtype=np.float32)
