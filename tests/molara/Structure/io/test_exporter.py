8616b97b0de5ffcb9b10d8c1ce04df2f7829e930
Adrian Usler <adrian.usler@googlemail.com>
2024-03-11 22:11:41 +0100

    Add test of GeneralExporter

--- tests/molara/Structure/io/test_exporter.py
+++ tests/molara/Structure/io/test_exporter.py
@@ -7,7 +7,7 @@ import unittest
 from pathlib import Path
 
 import numpy as np
-from molara.Structure.io.exporter import XyzExporter
+from molara.Structure.io.exporter import GeneralExporter, XyzExporter
 from molara.Structure.structure import Structure
 from numpy.testing import assert_array_equal
 
@@ -58,28 +58,53 @@ class TestXyzExporter(unittest.TestCase):
         assert_array_equal(coordinates, self.coordinates)
 
         # Delete file
-        Path(f"output_temporary_{self.timestamp}.xyz").unlink()
+        Path(self.filename).unlink()
 
 
-# class TestGeneralExporter(unittest.TestCase):
-#     """This class contains the tests for the GeneralExporter class."""
+class TestGeneralExporter(unittest.TestCase):
+    """This class contains the tests for the GeneralExporter class."""
 
-#     def test_write_structure_with_xyz_file(self) -> None:
-#         """Tests the write_structure method of the GeneralExporter class with an XYZ file."""
-#         exporter = GeneralExporter("output.xyz")
-#         structure = Structure()
-#         # Add atoms to the structure
-#         # ...
-#         exporter.write_structure(structure)
-#         # Assert that the output file exists
-#         assert Path("output.xyz").exists()
+    def setUp(self) -> None:
+        """Instantiates the GeneralExporter object."""
+        self.timestamp = int(time.time())
+        self.filename_xyz = f"output_temporary_{self.timestamp}.xyz"
+        self.exporter_xyz = GeneralExporter(self.filename_xyz)
+        self.atomic_numbers = np.array([9, 10, 17, 30, 15, 3])  # F, Ne, Cl, Zn, P, Li
+        self.coordinates = np.array(
+            [
+                [7.0, 3.0, 9.0],  # F
+                [1.3, -20.0, -300.0],  # Ne
+                [4.0e-3, 5.0e-4, 6.0e-2],  # Cl
+                [10.0, 11.0, 12.0],  # Zn
+                [13.2, 14.1, 15.123],  # P
+                [0, 0, 0],  # Li
+            ],
+        )
+        self.structure = Structure(
+            atomic_numbers=self.atomic_numbers,
+            coordinates=self.coordinates,
+        )
 
-#     def test_write_structure_with_unknown_file_format(self) -> None:
-#         """Tests the write_structure method of the GeneralExporter class with an unknown file format."""
-#         exporter = GeneralExporter("output.txt")
-#         structure = Structure()
-#         # Add atoms to the structure
-#         # ...
-#         # Assert that a FileFormatError is raised
-#         with pytest.raises(FileFormatError):
-#             exporter.write_structure(structure)
+    def test_write_structure_to_xyz(self) -> None:
+        """Tests the write_structure method of the GeneralExporter class."""
+        assert Path(self.filename_xyz).exists() is False
+        self.exporter_xyz.write_structure(self.structure)
+        # Assert that the output file exists
+        assert Path(self.filename_xyz).exists()
+
+        # Assert that the output file has the expected content
+        with open(self.filename_xyz) as file:
+            num_atoms = int(file.readline().strip())
+            assert num_atoms == self.atomic_numbers.size
+        data = np.genfromtxt(
+            self.filename_xyz,
+            skip_header=2,
+            dtype=str,
+        )
+        assert data.shape == (6, 4)
+        assert data[:, 0].tolist() == ["F", "Ne", "Cl", "Zn", "P", "Li"]
+        coordinates = data[:, 1:].astype(float)
+        assert_array_equal(coordinates, self.coordinates)
+
+        # Delete file
+        Path(self.filename_xyz).unlink()
