c398a2f2dedbc27c5b69622e38160bf46f34ff8b
Michel Heinz <heinz@pc.rwth-aachen.de>
2024-03-14 09:28:25 +0100

    Fix overwrite sys.argv with unittest.mock.patch

--- tests/molara/Gui/test_main_window.py
+++ tests/molara/Gui/test_main_window.py
@@ -22,6 +22,7 @@ if TYPE_CHECKING:
     from pytestqt.qtbot import QtBot
 
 import sys
+import unittest.mock as mock
 
 
 class WorkaroundTestMainWindow:
@@ -160,13 +161,15 @@ class WorkaroundTestMainWindow:
 
     def test_show_init_xyz(self) -> None:
         """Write test code to verify the behavior of show_init_xyz method."""
-        sys.argv[1] = "examples/xyz/pentane.xyz"
-        window = self.window
-        window.show_init_xyz()
-        assert isinstance(window.mols, Molecules)
-        assert isinstance(window.structure_widget.structure, Molecule)
-        assert window.structure_widget.structure_is_set
-        assert window.structure_widget.structure is window.mols.get_current_mol()
+        testargs = ["molara", "examples/xyz/pentane.xyz"]
+        with mock.patch.object(sys, 'argv', testargs):
+            print(sys.argv)
+            window = self.window
+            window.show_init_xyz()
+            assert isinstance(window.mols, Molecules)
+            assert isinstance(window.structure_widget.structure, Molecule)
+            assert window.structure_widget.structure_is_set
+            assert window.structure_widget.structure is window.mols.get_current_mol()
 
     def test_load_molecules(self) -> None:
         """Write test code to verify the behavior of load_molecules method."""
