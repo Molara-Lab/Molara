b1a27bf5af9bc4809f5ce941bd60739864a17581
Adrian Usler <adrian.usler@googlemail.com>
2024-03-29 02:50:28 +0100

    Restructure builder dialog code

--- src/molara/Gui/builder.py
+++ src/molara/Gui/builder.py
@@ -46,7 +46,7 @@ class BuilderDialog(QDialog):
     def __init__(self, parent: QMainWindow = None) -> None:
         """Initialize the ZMatBuilder dialog.
 
-        :param parent: QOpenGLWidget: The structure widget.
+        :param parent: the parent widget (main window).
         """
         super().__init__(
             parent,
@@ -66,6 +66,7 @@ class BuilderDialog(QDialog):
         self.z_matrix: list[dict] = []
 
         self.disable_slot: bool = False
+        self.colliding_idx: int | None = None
 
         self._initialize_table()
 
@@ -89,29 +90,20 @@ class BuilderDialog(QDialog):
         self.colliding_idx = None
 
         if self.main_window.mols.num_mols == 0:
-            params, atom_nums = self._get_parameters(0)
-
             self.disable_slot = False
-            self.add_first_atom(params)
-            mol = self.main_window.mols.mols[0]
+            num_atoms = 0
         else:
-            mol = self.main_window.mols.mols[0]
-            params, atom_nums = self._get_parameters(mol.n_at)
+            num_atoms = self.main_window.mols.mols[0].n_at
+        params, atom_ids = self._get_parameters(num_atoms)
 
-            if mol.n_at == 0:
-                self.add_first_atom(params)
-            elif mol.n_at == 1:
-                self.add_second_atom(mol, params)
-            elif mol.n_at == 2:  # noqa: PLR2004
-                self.add_third_atom(mol, params, atom_nums)
-            else:
-                self.add_nth_atom(mol, params, atom_nums)
+        self.add_atom(num_atoms, params, atom_ids)
+        mol = self.main_window.mols.mols[0]
 
         self.disable_slot = False
         if not self.err and self.colliding_idx is None:
             self.structure_widget.set_structure(mol)
             self.structure_widget.update()
-            self.z_matrix.append({"parameter": params, "atom_nums": atom_nums})
+            self.z_matrix.append({"parameter": params, "atom_ids": atom_ids})
             self._set_z_matrix_row(mol.n_at, mol.n_at - 1)
             self.structure_widget.clear_builder_selected_atoms()
 
@@ -163,46 +155,33 @@ class BuilderDialog(QDialog):
             error_msg = "Incorrect input type."
             self.ui.ErrorMessageBrowser.setText(error_msg)
             self._set_z_matrix_row(self.main_window.mols.mols[0].n_at, row)
+            return
 
-        else:
-            self.z_matrix_temp = deepcopy(self.z_matrix)
+        self.z_matrix_temp = deepcopy(self.z_matrix)
+        self.z_matrix[row]["parameter"] = params
+        mol_temp = self.main_window.mols.mols[0]
+        self.main_window.mols.remove_molecule(0)
 
-            self.z_matrix[row]["parameter"] = params
+        for i in range(len(self.z_matrix)):
+            params = self.z_matrix[i]["parameter"]
+            atom_ids = self.z_matrix[i]["atom_ids"]
+            if i == 0:
+                self.disable_slot = False
+            self.add_atom(i, params, atom_ids)
+            mol: Molecule = self.main_window.mols.mols[0]
 
-            mol_temp = self.main_window.mols.mols[0]
+            if self.colliding_idx is not None:
+                mol = mol_temp
+                self.z_matrix = self.z_matrix_temp
+                self.main_window.mols.mols[0] = mol_temp
+                break
 
-            self.main_window.mols.remove_molecule(0)
+        self.structure_widget.delete_structure()
+        self.structure_widget.set_structure(mol)
 
-            for i in range(len(self.z_matrix)):
-                if i == 0:
-                    params = self.z_matrix[0]["parameter"]
-                    atom_nums = self.z_matrix[0]["atom_nums"]
-                    self.disable_slot = False
-                    self.add_first_atom(params)
-                    mol: Molecule = self.main_window.mols.mols[0]
-                else:
-                    mol = self.main_window.mols.mols[0]
-                    params = self.z_matrix[i]["parameter"]
-                    atom_nums = self.z_matrix[i]["atom_nums"]
-                    if i >= 3:  # noqa: PLR2004
-                        self.add_nth_atom(mol, params, atom_nums)
-                    elif i == 2:  # noqa: PLR2004
-                        self.add_third_atom(mol, params, atom_nums)
-                    elif i == 1:
-                        self.add_second_atom(mol, params)
+        self._update_z_matrix(mol.n_at)
 
-                if self.colliding_idx is not None:
-                    mol = mol_temp
-                    self.z_matrix = self.z_matrix_temp
-                    self.main_window.mols.mols[0] = mol_temp
-                    break
-
-            self.structure_widget.delete_structure()
-            self.structure_widget.set_structure(mol)
-
-            self._update_z_matrix(mol.n_at)
-
-            self.colliding_idx = None
+        self.colliding_idx = None
 
     def _orth(self, vec: np.ndarray, unitvec: np.ndarray) -> np.ndarray:
         """Calculate a vector that is orthogonal to the two given vectors.
@@ -213,97 +192,95 @@ class BuilderDialog(QDialog):
         """
         return vec - np.dot(vec, unitvec) * unitvec
 
-    @toggle_slot
-    def add_first_atom(self, params: tuple) -> None:
-        """Initialize a molecule and adds the first atom to it.
+    def add_atom(self, count_atoms: int, params: tuple, atom_ids: list) -> None:
+        """Add an atom to the molecule.
 
-        :param params: parameters that are passed for the first atom
+        :param count_atoms: number of atoms that have been added so far
+        :param params: parameters that are passed for the atom
+        :param atom_ids: atom ids of the selected atoms
         """
-        element, _ = params
-        at_chrg = element_symbol_to_atomic_number(element)
-        at_chrg_check = self._check_element(at_chrg)
+        element, dist = params[0], params[1]
+        angle = params[2] if len(params) > 2 else None  # noqa: PLR2004
+        # dihedral = params[3] if len(params) > 3 else None
 
-        init_xyz = np.zeros([1, 3])
+        atomic_num = element_symbol_to_atomic_number(element)
+        atomic_num_valid = self._check_element(atomic_num)
+        dist_valid = self._check_value(dist) if count_atoms > 0 else True
+        angle_valid = self._check_value(angle) if angle is not None else True
+        values_valid = atomic_num_valid and dist_valid and angle_valid
 
-        self.main_window.mols.add_molecule(Molecule(np.array([at_chrg]), init_xyz, draw_bonds=False))
-        if at_chrg_check:
-            self.ui.tableWidget.setRowCount(1)
-            self.ui.tableWidget.setItem(0, 0, QTableWidgetItem(element))
+        if not (atomic_num_valid and values_valid):
+            return
 
-    def add_second_atom(self, mol: Molecule, params: tuple) -> None:
-        """Add a second atom.
+        if count_atoms == 0:
+            pos = self.calc_position_new_atom(count_atoms, params, atom_ids)
+            self.main_window.mols.add_molecule(
+                Molecule(np.array([atomic_num]), pos, draw_bonds=False),
+            )
+            return
 
-        :param mol: The molecule where a second atom shall be added.
+        # starting from the third atom, atoms must have been selected in order to specify relative position
+        atom_selection_check = self._check_selected_atoms(*atom_ids[:count_atoms]) if count_atoms >= 2 else True  # noqa: PLR2004
+        if not atom_selection_check:
+            return
+
+        mol = self.main_window.mols.mols[0]
+        pos = self.calc_position_new_atom(count_atoms, params, atom_ids)
+
+        # check for collisions
+        self.colliding_idx = mol.compute_collision(pos) if count_atoms >= 3 else None  # noqa: PLR2004
+        if self.colliding_idx is not None:
+            error_msg = f"The atom would collide with atom {self.colliding_idx+1}"
+            self.ui.ErrorMessageBrowser.setText(error_msg)
+            return
+
+        mol.toggle_bonds() if count_atoms == 1 else None
+        mol.add_atom(atomic_num, np.squeeze(pos))
+
+    def calc_position_new_atom(self, count_atoms: int, params: tuple, atom_ids: list) -> np.ndarray:
+        """Calculate the position of the new atom to be added.
+
+        :param count_atoms: number of atoms that have been added so far
+        :param params: parameters that are passed for the atom
+        :param atom_ids: atom ids of the selected atoms
         """
-        element, dist = params
-        at_chrg = element_symbol_to_atomic_number(element)
-        at_chrg_check = self._check_element(at_chrg)
-        boundary_check = self._check_value(dist)
-        if boundary_check and at_chrg_check:
-            coord = np.array([0.0, 0.0, dist])
-            mol.toggle_bonds()
-            mol.add_atom(at_chrg, coord)
+        if count_atoms == 0:
+            return np.zeros([1, 3])
 
-    def add_third_atom(self, mol: Molecule, params: tuple, atom_ids: list) -> None:
-        """Add a third atom to the molecule.
+        dist = params[1]
+        angle = params[2] if len(params) > 2 else None  # noqa: PLR2004
+        dihedral = params[3] if len(params) > 3 else None  # noqa: PLR2004
+        # add second atom
+        if count_atoms == 1:
+            return np.array([0.0, 0.0, dist])
 
-        :param mol: The molecule where a second atom shall be added.
-        :param params: atom parameters (element, distance to selected atom, angle to selected bond)
-        """
-        element, dist, angle = params
-        at1_id: int = atom_ids[0]
-        at2_id: int = atom_ids[1]
-        at_chrg = element_symbol_to_atomic_number(element)
-        boundary_check = self._check_value(dist, angle)
-        atom_selection_check = self._check_selected_atoms(at1_id, at2_id)
-        at_chrg_check = self._check_element(at_chrg)
-
-        if boundary_check and atom_selection_check and at_chrg_check:
+        mol = self.main_window.mols.mols[0]
+        # add third atom
+        if count_atoms == 2:  # noqa: PLR2004
             coord = np.array([dist * np.sin(angle), 0, dist * np.cos(angle)])
-            if at1_id == 1:
-                coord[2] = mol.atoms[at1_id].position[2] - coord[2]
-            else:
-                coord[2] = mol.atoms[at1_id].position[2] + coord[2]
+            coord[2] = (
+                mol.atoms[atom_ids[0]].position[2] - coord[2]
+                if atom_ids[0] == 1
+                else mol.atoms[atom_ids[0]].position[2] + coord[2]
+            )
+            return coord
 
-            mol.add_atom(at_chrg, coord)
-
-    def add_nth_atom(self, mol: Molecule, params: tuple, atom_nums: list) -> None:
-        """Add an nth atom to the molecule.
-
-        :param mol: The molecule where an nth atom shall be added.
-        :param params: atom parameters (element, distance to selected atom, angle to selected bond)
-
-        """
-        element, dist, angle, dihedral = params
-        at1_num: int = atom_nums[0]
-        at2_num: int = atom_nums[1]
-        at3_num: int = atom_nums[2]
-        at_chrg = element_symbol_to_atomic_number(element)
-        boundary_check = self._check_value(dist, angle)
-        atom_selection_check = self._check_selected_atoms(at1_num, at2_num, at3_num)
-        at_chrg_check = self._check_element(at_chrg)
-
-        if boundary_check and atom_selection_check and at_chrg_check:
-            vec1 = mol.atoms[at2_num].position - mol.atoms[at1_num].position
-            length = np.linalg.norm(vec1)
-            vec1 = vec1 / length if length > 0 else np.array([0, 0, 1.0])
-            vec2 = self._orth(mol.atoms[at3_num].position - mol.atoms[at2_num].position, vec1)
-            length = np.linalg.norm(vec2)
-            vec2 = vec2 / length if length > 0 else np.array([1.0, 0, 0])
-            if np.linalg.norm(self._orth(vec2, vec1)) == 0:
-                vec2 = np.array([0, 1.0, 0])
-            vec3 = np.cross(vec1, vec2)
-            vec3 = vec3 / np.linalg.norm(vec3)
-            tmp = dist * np.sin(angle)
-            coord = mol.atoms[at1_num].position + dist * np.cos(angle) * vec1
-            coord += tmp * np.cos(dihedral) * vec2 + tmp * np.sin(dihedral) * vec3
-
-            self.colliding_idx = mol.compute_collision(coord)
-            if self.colliding_idx is None:
-                mol.add_atom(at_chrg, np.squeeze(coord))
-            else:
-                error_msg = f"The atom would collide with atom {self.colliding_idx+1}"
-                self.ui.ErrorMessageBrowser.setText(error_msg)
+        # add nth atom
+        at1_id, at2_id, at3_id = atom_ids
+        vec1 = mol.atoms[at2_id].position - mol.atoms[at1_id].position
+        length = np.linalg.norm(vec1)
+        vec1 = vec1 / length if length > 0 else np.array([0, 0, 1.0])
+        vec2 = self._orth(mol.atoms[at3_id].position - mol.atoms[at2_id].position, vec1)
+        length = np.linalg.norm(vec2)
+        vec2 = vec2 / length if length > 0 else np.array([1.0, 0, 0])
+        if np.linalg.norm(self._orth(vec2, vec1)) == 0:
+            vec2 = np.array([0, 1.0, 0])
+        vec3 = np.cross(vec1, vec2)
+        vec3 = vec3 / np.linalg.norm(vec3)
+        tmp = dist * np.sin(angle)
+        coord = mol.atoms[at1_id].position + dist * np.cos(angle) * vec1
+        coord += tmp * np.cos(dihedral) * vec2 + tmp * np.sin(dihedral) * vec3
+        return coord
 
     def _clear_all_text(self) -> None:
         """Clear all text in the zbuilder dialog."""
@@ -349,14 +326,14 @@ class BuilderDialog(QDialog):
 
         return all_selected
 
-    def _check_element(self, at_chrg: int | None) -> bool:
+    def _check_element(self, atomic_num: int | None) -> bool:
         """Check if the element which should be added exists.
 
         :param at_charg: atomic charge which is None if not an element.
         """
         error_msg = "This is not an element."
         is_element = True
-        if at_chrg == 0:
+        if atomic_num == 0:
             is_element = False
             self.ui.ErrorMessageBrowser.setText(error_msg)
             self.err = True
@@ -386,7 +363,7 @@ class BuilderDialog(QDialog):
 
                 self.ui.tableWidget.setItem(row, param_rows[i], QTableWidgetItem(temp_text))
                 if param_rows[i] != 0:
-                    atom_id = self.z_matrix[row]["atom_nums"][i - 1]
+                    atom_id = self.z_matrix[row]["atom_ids"][i - 1]
                     self.ui.tableWidget.setItem(row, atom_id_rows[i], QTableWidgetItem(str(atom_id + 1)))
 
     def _update_z_matrix(self, tot_row: int) -> None:
@@ -414,20 +391,20 @@ class BuilderDialog(QDialog):
         dist: float = float(self.ui.Box_1BondDistance.text())
         angle: float = np.deg2rad(float(self.ui.Box_2BondAngle.text()))
         dihedral: float = np.deg2rad(float(self.ui.Box_3DihedralAngle.text()))
-        atom_nums = self.structure_widget.builder_selected_spheres
+        atom_ids = self.structure_widget.builder_selected_spheres
 
         if num_atoms == 0:
             return (element, None), []
         if num_atoms == 1:
             return (element, dist), [0]
         if num_atoms == 2:  # noqa: PLR2004
-            return (element, dist, angle), atom_nums
+            return (element, dist, angle), atom_ids
         return (
             element,
             dist,
             angle,
             dihedral,
-        ), atom_nums
+        ), atom_ids
 
     def _get_parameters_from_table(self, row: int) -> tuple:
         """Return the parameter of a specified row in the table.
@@ -479,7 +456,7 @@ class BuilderDialog(QDialog):
 
         else:
             for entry in self.z_matrix:
-                if idx in entry["atom_nums"]:
+                if idx in entry["atom_ids"]:
                     error_msg = f"Cannot be deleted. Atom {idx+1} depends on this atom."
                     do_deletion = False
                     self.ui.ErrorMessageBrowser.setText(error_msg)
@@ -493,9 +470,9 @@ class BuilderDialog(QDialog):
         :param num_atoms: Total number of atoms in the molecule.
         """
         for i in range(idx, num_atoms):
-            for j, at_num in enumerate(self.z_matrix[i]["atom_nums"]):
+            for j, at_num in enumerate(self.z_matrix[i]["atom_ids"]):
                 if at_num > idx and at_num != -1:
-                    self.z_matrix[i]["atom_nums"][j] -= 1
+                    self.z_matrix[i]["atom_ids"][j] -= 1
 
         self.z_matrix.pop(idx)
 
