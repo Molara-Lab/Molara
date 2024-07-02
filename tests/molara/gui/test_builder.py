8a4f68e769186f603a8bd2ed586d6701534fd4f2
Alex Bonkowski <alexander.bonkowski@rwth-aachen.de>
2024-07-02 20:36:50 +0200

    Add `select_add' test

--- tests/molara/gui/test_builder.py
+++ tests/molara/gui/test_builder.py
@@ -27,6 +27,7 @@ class WorkaroundTestBuilderDialog:
     def run_tests(self) -> None:
         """Run the tests."""
         self._test_init()
+        self._test_select_add()
 
     def _test_init(self) -> None:
         """Test the initialization of the BuilderDialog class."""
@@ -34,3 +35,11 @@ class WorkaroundTestBuilderDialog:
         builder_dialog = self.builder_dialog
         builder_dialog.show()
         assert builder_dialog.isVisible()
+    
+    def _test_select_add(self) -> None:
+        """Test the selection of the add button."""
+        builder_dialog = self.builder_dialog
+        builder_dialog.select_add()
+        assert builder_dialog.err == True
+        assert builder_dialog.colliding_idx == None
+
