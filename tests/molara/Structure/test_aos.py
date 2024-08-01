dc311e7b5405930fddba33718025fbe1f7c34b95
Adrian Usler <adrian.usler@googlemail.com>
2024-08-01 22:32:25 +0200

    Extend testing of aos.py

--- tests/molara/Structure/test_aos.py
+++ tests/molara/Structure/test_aos.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from unittest import TestCase
 
 import numpy as np
+from numpy.testing import assert_array_almost_equal
 from molara.Eval.aos import calculate_aos
 from molara.Structure.io.importer import GeneralImporter
 
@@ -27,30 +28,295 @@ class TestAos(TestCase):
 
         Tests still need to be implemented.
         """
-        s = []
-        p = []
-        pi = 0
+        quantum_number_l = {
+            "s": 0,
+            "p": 1,
+            "d": 2,
+            "f": 3,
+            "g": 4,
+        }
+        orbital_array_lengths = {
+            "s": 1,
+            "p": 3,
+            "d": 6,
+            "f": 10,
+            "g": 15,
+        }
+        # orbital_lists = {
+        #     "s": [],
+        #     "p": [],
+        #     "d": [],
+        #     "f": [],
+        #     "g": [],
+        # }
+        # pi = 0
+
+        def _test_orbital(orb: str) -> None:
+            """Test the orbital."""
+            orbital_type = orb[0]
+            current_orbital = calculate_aos(
+                self.electron_position,
+                self.nuclear_position,
+                self.basisset.orbitals[orb].exponents,
+                self.basisset.orbitals[orb].coefficients,
+                quantum_number_l[orbital_type],
+            )
+            compare_orbital = reference_calculate_aos(
+                self.electron_position,
+                self.nuclear_position,
+                self.basisset.orbitals[orb].exponents,
+                self.basisset.orbitals[orb].coefficients,
+                quantum_number_l[orbital_type],
+            )
+            compare_orbital_old_implementation = reference2_calculate_aos(
+                self.electron_position,
+                self.nuclear_position,
+                self.basisset.orbitals[orb].exponents,
+                self.basisset.orbitals[orb].coefficients,
+                quantum_number_l[orbital_type],
+            )
+            assert current_orbital.size == orbital_array_lengths[orbital_type]
+            assert_array_almost_equal(current_orbital, compare_orbital)
+            assert_array_almost_equal(current_orbital, compare_orbital_old_implementation)
+            # orbital_lists[orbital_type].append(current_orbital)
 
         for orb in self.basisset.orbitals:
-            if orb[0] == "s":
-                s.append(
-                    calculate_aos(
-                        self.electron_position,
-                        self.nuclear_position,
-                        self.basisset.orbitals[orb].exponents,
-                        self.basisset.orbitals[orb].coefficients,
-                        0,
-                    ),
-                )
-            elif orb[0] == "p" and pi % 3 == 0:
-                p.append(
-                    calculate_aos(
-                        self.electron_position,
-                        self.nuclear_position,
-                        self.basisset.orbitals[orb].exponents,
-                        self.basisset.orbitals[orb].coefficients,
-                        1,
-                    ),
-                )
-                pi += 1
-        # assert False
+            _test_orbital(orb)
+
+def reference_calculate_aos(
+    electron_coords: np.ndarray,
+    atom_coords: np.ndarray,
+    exponents: np.ndarray,
+    coefficients: np.ndarray,
+    orbital: int,
+) -> np.ndarray:
+    """Calculate the atomic orbitals for a given atom (cartesian).
+
+    If the orbital is an s orbital, the function the return has size 1, if the orbital is a d orbital, the return
+    ha size 6 (dxx, dyy, dzz, etc.).
+    :param electron_coords: list of coordinates
+    :param atom_coords: list of atom coordinates
+    :param exponents: list of exponents
+    :param coefficients: list of coefficients
+    :param orbital: Name of the orbital (ie, s, p, d, etc.)
+    :return: Value of the atomic orbital(s) for a given atom position and electron position
+    """
+    sqr3 = 1.73205080756887729
+    sqr5 = 2.236067977499789696
+    sqr7 = 2.645751311064591
+    s, p, d, f, g = 0, 1, 2, 3, 4
+
+    ngto = len(exponents)
+    relative_coords = electron_coords - atom_coords
+    dx, dy, dz = relative_coords
+    dx2, dy2, dz2 = dx**2, dy**2, dz**2
+    dxyz = dx * dy * dz
+    rr = np.linalg.norm(relative_coords)
+    r2 = rr**2
+    if orbital == s:
+        uao = np.zeros(1)
+        directional_factors = np.array([1.])
+    elif orbital == p:
+        uao = np.zeros(3)
+        directional_factors = np.array([dx, dy, dz])
+    elif orbital == d:
+        uao = np.zeros(6)
+        directional_factors = np.array([
+            dx2,
+            dy2,
+            dz2,
+            sqr3*dx*dy,
+            sqr3*dx*dz,
+            sqr3*dy*dz,
+        ])
+    elif orbital == f:
+        uao = np.zeros(10)
+        directional_factors = np.array([
+            dx2*dx,# fxxx
+            dy2*dy,# fyyy
+            dz2*dz,# fzzz
+            sqr5*dx2*dy,# fxyy
+            sqr5*dx2*dz,# fxxy
+            sqr5*dy2*dx,# fxxz
+            sqr5*dy2*dz,# fxzz
+            sqr5*dz2*dx,# fyyz
+            sqr5*dz2*dy,# fyzz
+            sqr5*sqr3*dxyz,# fxyz
+        ])
+    elif orbital == g:
+        uao = np.zeros(15)
+        directional_factors = np.array([
+            dx2 * dx2,
+            dy2 * dy2,
+            dz2 * dz2,
+            sqr7*dx2 * dx * dy,
+            sqr7*dx2 * dx * dz,
+            sqr7*dy2 * dy * dx,
+            sqr7*dy2 * dy * dz,
+            sqr7*dz2 * dz * dx,
+            sqr7*dz2 * dz * dy,
+            sqr7*sqr5/sqr3*dx2 * dy2,
+            sqr7*sqr5/sqr3*dx2 * dz2,
+            sqr7*sqr5/sqr3*dy2 * dz2,
+            sqr7*sqr5*dx * dxyz,
+            sqr7*sqr5*dy * dxyz,
+            sqr7*sqr5*dz * dxyz,
+        ])
+    else:
+        msg = "(calculate_aos): wrong GTO"
+        raise TypeError(msg)
+    for ic in range(ngto):
+        u = coefficients[ic] * np.exp(-exponents[ic] * r2)
+        uao += directional_factors * u
+    return uao
+
+# Long-term: delete this function
+# The function is used to compare the results of the new implementation with the old one.
+# Once we are confident that the new implementation is correct, we can delete this function.
+def reference2_calculate_aos(# noqa: C901 PLR0915
+    electron_coords: np.ndarray,
+    atom_coords: np.ndarray,
+    exponents: np.ndarray,
+    coefficients: np.ndarray,
+    orbital: int,
+) -> np.ndarray:
+    """Calculate the atomic orbitals for a given atom (cartesian).
+
+    If the orbital is an s orbital, the function the return has size 1, if the orbital is a d orbital, the return
+    ha size 6 (dxx, dyy, dzz, etc.).
+    :param electron_coords: list of coordinates
+    :param atom_coords: list of atom coordinates
+    :param exponents: list of exponents
+    :param coefficients: list of coefficients
+    :param orbital: Name of the orbital (ie, s, p, d, etc.)
+    :return: Value of the atomic orbital(s) for a given atom position and electron position
+    """
+    sqr3 = 1.73205080756887729
+    sqr5 = 2.236067977499789696
+    sqr7 = 2.645751311064591
+
+    s = 0
+    p = 1
+    d = 2
+    f = 3
+    g = 4
+
+    fxxx = 0
+    fyyy = 1
+    fzzz = 2
+    fxyy = 3
+    fxxy = 4
+    fxxz = 5
+    fxzz = 6
+    fyzz = 7
+    fyyz = 8
+    fxyz = 9
+
+    gxxxx = 0
+    gyyyy = 1
+    gzzzz = 2
+    gxxxy = 3
+    gxxxz = 4
+    gyyyx = 5
+    gyyyz = 6
+    gzzzx = 7
+    gzzzy = 8
+    gxxyy = 9
+    gxxzz = 10
+    gyyzz = 11
+    gxxyz = 12
+    gyyxz = 13
+    gzzxy = 14
+
+    ngto = len(exponents)
+    relative_coords = electron_coords - atom_coords
+    rr = np.linalg.norm(relative_coords)
+    r2 = rr * rr
+    if orbital == s:
+        uao = np.zeros(1)
+        for ic in range(ngto):
+            u = coefficients[ic] * np.exp(-exponents[ic] * r2)
+            uao[0] = uao[0] + u
+    elif orbital == p:
+        uao = np.zeros(3)
+        for ic in range(ngto):
+            u = coefficients[ic] * np.exp(-exponents[ic] * r2)
+            dx = relative_coords[0]
+            dy = relative_coords[1]
+            dz = relative_coords[2]
+            uao[0] = uao[0] + dx * u
+            uao[1] = uao[1] + dy * u
+            uao[2] = uao[2] + dz * u
+    elif orbital == d:
+        uao = np.zeros(6)
+        for ic in range(ngto):
+            u = coefficients[ic] * np.exp(-exponents[ic] * r2)
+            dx = relative_coords[0]
+            dx2 = dx * dx
+            dy = relative_coords[1]
+            dy2 = dy * dy
+            dz = relative_coords[2]
+            dz2 = dz * dz
+            uao[0] = uao[0] + dx2 * u
+            uao[1] = uao[1] + dy2 * u
+            uao[2] = uao[2] + dz2 * u
+            u = sqr3 * u
+            uao[3] = uao[3] + dx * dy * u
+            uao[4] = uao[4] + dx * dz * u
+            uao[5] = uao[5] + dy * dz * u
+    elif orbital == f:
+        uao = np.zeros(10)
+        for ic in range(ngto):
+            u = coefficients[ic] * np.exp(-exponents[ic] * r2)
+            dx = relative_coords[0]
+            dx2 = dx * dx
+            dy = relative_coords[1]
+            dy2 = dy * dy
+            dz = relative_coords[2]
+            dz2 = dz * dz
+            dxyz = dx * dy * dz
+            uao[fxxx] = uao[fxxx] + dx2 * dx * u
+            uao[fyyy] = uao[fyyy] + dy2 * dy * u
+            uao[fzzz] = uao[fzzz] + dz2 * dz * u
+            u = sqr5 * u
+            uao[fxxy] = uao[fxxy] + dx2 * dy * u
+            uao[fxxz] = uao[fxxz] + dx2 * dz * u
+            uao[fxyy] = uao[fxyy] + dy2 * dx * u
+            uao[fyyz] = uao[fyyz] + dy2 * dz * u
+            uao[fxzz] = uao[fxzz] + dz2 * dx * u
+            uao[fyzz] = uao[fyzz] + dz2 * dy * u
+            u = sqr3 * u
+            uao[fxyz] = uao[fxyz] + dxyz * u
+    elif orbital == g:
+        uao = np.zeros(15)
+        for ic in range(ngto):
+            u = coefficients[ic] * np.exp(-exponents[ic] * r2)
+            dx = relative_coords[0]
+            dx2 = dx * dx
+            dy = relative_coords[1]
+            dy2 = dy * dy
+            dz = relative_coords[2]
+            dz2 = dz * dz
+            dxyz = dx * dy * dz
+            uao[gxxxx] = uao[gxxxx] + dx2 * dx2 * u
+            uao[gyyyy] = uao[gyyyy] + dy2 * dy2 * u
+            uao[gzzzz] = uao[gzzzz] + dz2 * dz2 * u
+            u = sqr7 * u
+            uao[gxxxy] = uao[gxxxy] + dx2 * dx * dy * u
+            uao[gxxxz] = uao[gxxxz] + dx2 * dx * dz * u
+            uao[gyyyx] = uao[gyyyx] + dy2 * dy * dx * u
+            uao[gyyyz] = uao[gyyyz] + dy2 * dy * dz * u
+            uao[gzzzx] = uao[gzzzx] + dz2 * dz * dx * u
+            uao[gzzzy] = uao[gzzzy] + dz2 * dz * dy * u
+            u = sqr5 / sqr3 * u
+            uao[gxxyy] = uao[gxxyy] + dx2 * dy2 * u
+            uao[gxxzz] = uao[gxxzz] + dx2 * dz2 * u
+            uao[gyyzz] = uao[gyyzz] + dy2 * dz2 * u
+            u = sqr3 * u
+            uao[gxxyz] = uao[gxxyz] + dx * dxyz * u
+            uao[gyyxz] = uao[gyyxz] + dy * dxyz * u
+            uao[gzzxy] = uao[gzzxy] + dz * dxyz * u
+    else:
+        msg = "(calculate_aos): wrong GTO"
+        raise TypeError(msg)
+    return uao
