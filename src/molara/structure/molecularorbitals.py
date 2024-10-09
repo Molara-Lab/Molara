Fix sign of transformation matrices

src/molara/structure/molecularorbitals.py
@@ -255,14 +255,14 @@ class MolecularOrbitals:
         transformation_f[f2s, fxyz] = 1
 
 # TODO AKS PROF. LÜCHOW WHY AMOLQC DOES THIS DIFFERENTLY? (|m| >= 3)
-        transformation_f[f3c, fxxx] = np.sqrt(10) / 4
+        transformation_f[f3c, fxxx] = -np.sqrt(10) / 4
 # sqrt(5) is taken into account when calculating the aos values
-        transformation_f[f3c, fxyy] = -3 * np.sqrt(2) / 4
+        transformation_f[f3c, fxyy] = 3 * np.sqrt(2) / 4
 
 # TODO AKS PROF. LÜCHOW WHY AMOLQC DOES THIS DIFFERENTLY? (|m| >= 3)
-        transformation_f[f3s, fyyy] = -np.sqrt(10) / 4
+        transformation_f[f3s, fyyy] = np.sqrt(10) / 4
 # sqrt(5) is taken into account when calculating the aos values
-        transformation_f[f3s, fxxy] = 3 * np.sqrt(2) / 4
+        transformation_f[f3s, fxxy] = -3 * np.sqrt(2) / 4
         self.t_sc_f = transformation_f
 
 # g orbitals
@@ -304,27 +304,27 @@ class MolecularOrbitals:
 
 # TODO AKS PROF. LÜCHOW WHY AMOLQC DOES THIS DIFFERENTLY? (|m| >= 3)
 # sqrt(7) is taken into account when calculating the aos values
-        transformation_g[g3c, gxxxz] = np.sqrt(10) / 4
+        transformation_g[g3c, gxxxz] = -np.sqrt(10) / 4
 # sqrt(7) * sqrt(5) is taken into account when calculating the aos values
-        transformation_g[g3c, gyyxz] = -3 * np.sqrt(2) / 4
+        transformation_g[g3c, gyyxz] = 3 * np.sqrt(2) / 4
 
 # TODO AKS PROF. LÜCHOW WHY AMOLQC DOES THIS DIFFERENTLY? (|m| >= 3)
 # sqrt(7) is taken into account when calculating the aos values
-        transformation_g[g3s, gyyyz] = -np.sqrt(10) / 4
+        transformation_g[g3s, gyyyz] = np.sqrt(10) / 4
 # sqrt(7) * sqrt(5) is taken into account when calculating the aos values
-        transformation_g[g3s, gxxyz] = 3 * np.sqrt(2) / 4
+        transformation_g[g3s, gxxyz] = -3 * np.sqrt(2) / 4
 
 # TODO AKS PROF. LÜCHOW WHY AMOLQC DOES THIS DIFFERENTLY? (|m| >= 3)
-        transformation_g[g4c, gxxxx] = np.sqrt(35) / 8
-        transformation_g[g4c, gyyyy] = np.sqrt(35) / 8
+        transformation_g[g4c, gxxxx] = -np.sqrt(35) / 8
+        transformation_g[g4c, gyyyy] = -np.sqrt(35) / 8
 # sqrt(7) * sqrt(5) / sqrt(3) is taken into account when calculating the aos values
-        transformation_g[g4c, gxxyy] = -3 * np.sqrt(3) / 4
+        transformation_g[g4c, gxxyy] = 3 * np.sqrt(3) / 4
 
 # TODO AKS PROF. LÜCHOW WHY AMOLQC DOES THIS DIFFERENTLY? (|m| >= 3)
 # sqrt(7) is taken into account when calculating the aos values
-        transformation_g[g4s, gxxxy] = np.sqrt(5) / 2
+        transformation_g[g4s, gxxxy] = -np.sqrt(5) / 2
 # sqrt(7) is taken into account when calculating the aos values
-        transformation_g[g4s, gyyyx] = -np.sqrt(5) / 2
+        transformation_g[g4s, gyyyx] = np.sqrt(5) / 2
         self.t_sc_g = transformation_g
 
     def spherical_to_cartesian_transformation(self,