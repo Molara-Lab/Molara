b3daa512db3589cc4966cf4f79de686ca8c51c59
Michel Heinz <heinz@pc.rwth-aachen.de>
2024-10-25 21:05:25 +0200

    Add test for g orbitals

--- tests/molara/eval/test_mos.py
+++ tests/molara/eval/test_mos.py
@@ -16,16 +16,28 @@ class TestMolecularOrbitals(TestCase):
 
     def setUp(self) -> None:
         """Initialize the program."""
+        pass
+
+    def test_mo_evaluation(self) -> None:
+        """Test the calculation of the MO for a given electron position."""
         path = "examples/molden/h2o.molden"
         importer = GeneralImporter(path)
         molecule = importer.load().mols[0]
         molecule.center_coordinates()
         self.mos = molecule.mos
         self.aos = molecule.basis_set
-
-    def test_mo_evaluation(self) -> None:
-        """Test the calculation of the MO for a given electron position."""
         val = self.mos.get_mo_value(1, self.aos, np.array([0.10154165, 0.465418564, -1.498185465]))
         # Generated after comparing the mos with the mos of multiwfn.
         reference = 0.009741653204777932
+        assert val == reference
+        
+        path = "examples/molden/SPDFG_orbitals.molden"
+        importer = GeneralImporter(path)
+        molecule = importer.load().mols[0]
+        molecule.center_coordinates()
+        self.mos = molecule.mos
+        self.aos = molecule.basis_set
+        val = self.mos.get_mo_value(20, self.aos, np.array([0.10154165, 0.465418564, -1.498185465]))
+        # Generated after comparing the mos with the mos of multiwfn.
+        reference = 0.00041210939470218426
         assert val == reference
